99b4adb82c4c73d8713b82f4af833f726844df94
Pillifino <dinodizon073100@gmail.com>
2022-03-25 11:39:23 -0700

    Added Javascript link to html and also created time function using moment to display the day and time in jjumbotron

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
 //Global Variables
-var timeDisplayEl = document.querySelectorAll('#time-display'); // Variable used for displaying time
-var saveButton = document.querySelectorAll('#save-event')
-var eventBox = document.querySelectorAll('#event-box')
-var addedEvent = document.querySelectorAll('#') //Variable used in add event function
+var saveButton = $('#save-event')
+var eventBox = $('#event-box')
+// var addedEvent = document.querySelectorAll('#') //Variable used in add event function
+
+var timeNine = document.querySelectorAll('#nine')
+var timeTen = document.querySelectorAll('#ten')
+var timeEleven = document.querySelectorAll('#eleven')
+var timeNoon = document.querySelectorAll('#twelve')
+var timeOne = document.querySelectorAll('#one')
+var timeTwo = document.querySelectorAll('#two')
+var timeThree = document.querySelectorAll('#three')
+var timeFour = document.querySelectorAll('#four')
+var timeFive = document.querySelectorAll('#five')
+
 //Local storage Variable
-var eventInput = localStorage.getItem('#eventInput') // typed event the will become added event output
+var eventInput = localStorage.getItem('#eventInput'); // typed event the will become added event output
 
 //Functions
 //Displays Past Time (Gray)
-function displayPastTime() {
-    var pastHours = moment().startOf('hour').fromNow(); // X Hours Ago
-    timeDisplayEl.text(pastHours);
-}
+// function displayPastTime() {
+//     var pastHours = moment().startOf('hour').fromNow(); // X Hours Ago
+//     timeDisplayEl.text(pastHours);
+// }
 
 //Displays Present Time (Red)
-function displayPresentTime() {
-    var currentDay = moment().format('dddd MMM Do') //daay of the week , month , and day
-    timeDisplayEl.text(currentDay)
+function displayTime() {
+    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a')); //Jquery selecting the current day ID in jumbotron
 }
+setInterval(displayTime, 1000);
 
+//Present Time Shows As Red
+$( document.body ).click(function() {
+    $( "container" ).each(function( i ) {
+      if (moment().format('LT')) {
+        this.style.color = "";
+      } else {
+        this.style.color = "";
+      }
+    });
+  });
+
+// displayFutureTime();
 //Displays Future Time (Green)
-function displayFutureTime() {
-    var futureHour = moment().endOf('day').fromNow(); // In X Hours
-    timeDisplayEl.text(futureHour)
-}
+// function displayFutureTime() {
+//     var futureHour = moment().endOf('day').fromNow(); // In X Hours
+//     timeDisplayEl.text(futureHour);
+// }
 
 //Usert Types event
-function typedEvent(){
+// function typedEvent(){
     //Insert Function Here 
-}
+// }
 
 //Save Typed in Event
-function saveEvent(){
-    addedEvent.textcontent = eventInput;
-    localStorage.setItem("eventInput", eventInput);
-}
+// function saveEvent(){
+//     addedEvent.textcontent = eventInput;
+//     localStorage.setItem('eventInput', eventInput);
+// }
 
 //Event Listeners
 //Add Event By clicking On Time Block
-eventBox.addEventListener('click', function(){
-    typedEvent();
-})
+// eventBox.addEventListener('click', function(){
+//     typedEvent();
+// })
 //Save Event By Clicking On Save Icon (Add To Local Storage)
-saveButton.addEventListener('click', function(){
-    saveEvent();
-})
+// saveButton.addEventListener('click', function(){
+//     saveEvent();
+// })
